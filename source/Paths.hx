26d64b7afe5227a8ee61eee034d7250ea16fb68f
tchcfhvgd <2220687506@qq.com>
2025-08-23 00:46:46 +0800

    forgot

--- source/Paths.hx
+++ source/Paths.hx
@@ -6,6 +6,8 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
 
+using StringTools;
+
 class Paths
 {
 	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
